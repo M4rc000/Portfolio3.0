61e7cf36f6d6a6392a552b717d85e4fb763f518a
unknown <you@example.com>
2022-08-05 20:16:06 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,99 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzWCBVL3ClV1Yc6C1v3Mp6AOlA-j_Lrq_rEEqhpMYUA4hNrvajnCDAbKvNgyBIRgyyx/exec';
+const form = document.forms['portfolio-contact-form']
+
+const btnSend = document.querySelector('.btn-send');
+const btnLoading = document.querySelector('.btn-loading');
+const myAlert = document.querySelector('.my-alert');
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  btnLoading.classList.toggle('d-none');
+  btnSend.classList.toggle('d-none');
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      btnLoading.classList.toggle('d-none');
+      btnSend.classList.toggle('d-none');
+      myAlert.classList.toggle('d-none');
+      form.reset();
+      console.log('Success!', response)
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+
+
+
+// PARALLAX EFFECT
+
+$(window).on('load', function(){
+  setTimeout(function(){
+    $('.hero .hero-left').addClass('hero-left-show');  
+  },250);
+
+  setTimeout(function(){
+    $('.cover-circle').addClass('cover-circle-show');  
+  },250);
+
+});
+
+
+
+$(window).scroll(function(){
+    
+  const wScroll = $(this).scrollTop();
+ 
+
+// Parallax About
+if(wScroll > $('.container-about').offset().top - 230 ){
+  $('.isi-about',function(){
+      setTimeout(function(){
+          $('.isi-about-left').addClass('isi-about-left-show');  
+      },120);
+  });  
+  $('.isi-about',function(){
+      setTimeout(function(){
+          $('.isi-about-right').addClass('isi-about-right-show');  
+      },120);
+  });  
+}
+
+if(wScroll > $('.container-skills').offset().top - 230 ){
+  $('.isi-skills',function(){
+      setTimeout(function(){
+          $('.isi-skills-1-left').addClass('isi-skills-1-left-show');  
+      },120);
+  });  
+
+  $('.isi-skills',function(){
+      setTimeout(function(){
+          $('.isi-skills-1-right').addClass('isi-skills-1-right-show');  
+      },120);
+  });  
+
+}
+
+  if(wScroll > $('.isi-skills').offset().top - 0 ){
+  $('.isi-skills',function(){
+      setTimeout(function(){
+          $('.isi-skills-2-left').addClass('isi-skills-2-left-show');  
+      },120);
+  });  
+
+  $('.isi-skills',function(){
+      setTimeout(function(){
+          $('.isi-skills-2-right').addClass('isi-skills-2-right-show');  
+      },120);
+  });  
+}
+
+
+});
+
+
+$( '#navbar .navbar-nav a' ).on('click', 
+function () {
+$( '#navbar .navbar-nav' ).find( 'li.active' )
+.removeClass( 'active' );
+$( this ).parent( 'li' ).addClass( 'active' );
+});
